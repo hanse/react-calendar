1c74efbb51e3648948ebba3775083252ff9526a3
flavioespinoza <flavio.espinoza@gmail.com>
2019-10-22 01:20:17 -0600

    removed @babel/plugin-transform-react-jsx

--- src/FlavioHook.jsx
+++ src/FlavioHook.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FlavioHook = ({ ...props }) => {
   const { ...prop } = props;
   return (
-    <section style={{padding: 96}}>
+    <section style={{padding: 47}}>
       <h1>Hello Flavio Hook Component</h1>
       {prop.subtitle ? <h5>{prop.subtitle}</h5> : null}
     </section>
