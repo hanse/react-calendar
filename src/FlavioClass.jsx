a967418526c1f34a2e47e3214f1415b3fe984d56
flavioespinoza <flavio.espinoza@gmail.com>
2019-10-22 01:27:20 -0600

    move @babel to devDependencies

--- src/FlavioClass.jsx
+++ src/FlavioClass.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class FlavioClass extends Component {
   render() {
     return (
-      <div style={{padding: 48}}>
-        <h1>Hello Flavio Class Component</h1>
+      <div style={{padding: 42}}>
+        <h1>Hello Flavio Class Component!!!</h1>
       </div>
     )
   }
--- src/FlavioHook.jsx
+++ src/FlavioHook.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const FlavioHook = ({ ...props }) => {
   const { ...prop } = props;
   return (
-    <section style={{padding: 47}}>
-      <h1>Hello Flavio Hook Component</h1>
+    <section style={{padding: 43}}>
+      <h1>Hello Flavio Hook Component!!!</h1>
       {prop.subtitle ? <h5>{prop.subtitle}</h5> : null}
     </section>
   );
